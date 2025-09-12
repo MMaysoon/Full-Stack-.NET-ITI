ff2efa04b53495cffcd5ccbde69b54f2d331ad27
MMaysoon <100812757+MMaysoon@users.noreply.github.com>
2025-09-12 14:16:58 +0300

    Add files via upload

---
+++ OOP/Lab_05/Shapes/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    internal class Triangle:Shape
+    {
+        public Triangle() : base() { }
+        
+
+        public Triangle(int _dim) : base(_dim) { }
+
+        public Triangle(int _dim1, int _dim2) : base(_dim1, _dim2) { }
+
+        public override void CalArea()
+        {
+            Console.WriteLine($"Triangle Area = {0.5* GetDim1() * GetDim2()}");
+        }
+    }
+}
---
+++ OOP/Lab_05/Singleton/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Student
+    {
+        int id;
+        string name;
+
+        // 1- private static feild to hold the single instance 
+        private static Student student=null;
+
+        // 2- Private Ctor to bock "new"
+        private Student(int _id, string _name)
+        {
+           id = _id;
+          name = _name;
+        }
+
+        // 3- Public static method 
+        public static Student CreateStudent (int _id, string _name)
+        {
+            if (student == null)
+            {
+                student=new Student(_id, _name);
+            }
+            return student;
+        }
+
+        public override string ToString()
+        {
+            return $"Student with Id:{id} and name:{name}";
+        }
+    }
+}
