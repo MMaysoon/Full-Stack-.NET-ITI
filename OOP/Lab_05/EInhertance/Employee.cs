2a2a6ad4948045662df2407e67d07442c71a70d4
MMaysoon <100812757+MMaysoon@users.noreply.github.com>
2025-09-12 14:15:46 +0300

    Add files via upload

---
+++ OOP/Lab_05/EInhertance/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EInhertance
+{
+    internal class Employee :Person
+    {
+        int salary;
+
+        public void SetSalary(int _salary) { salary = _salary; }
+
+        public int GetSalary() { return salary; }
+
+        public Employee() : base()
+        {
+            salary = 1000;
+        }
+        // ctor with inital value 
+        public Employee(int _id = -1, string _name = "No Employee", int _age = -1, int _salary = 1000) : base(_id, _name, _age)
+        {
+            SetSalary(_salary);
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Employess :- {GetId()}   {GetName()}   {GetAge()}   {GetSalary()}");
+        }
+    }
+}
